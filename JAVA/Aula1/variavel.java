35a6f119b06153e73c4ec42e4e8acf01ddf63a05
eMlock11 <hyuke0309@gmail.com>
2025-12-04 22:07:49 -0300

    oafojaf

---
+++ JAVA/Aula1/variavel.java
@@ -0,0 +1,31 @@
+package Aula1;
+
+public class variavel {
+    public static void main(String[] args) {
+        String nome_foda;
+        int x,y;
+        String coisa2;
+        int olhaquelegal,quebacana;
+        String empolgante;
+
+        empolgante = "Matheus olha pro seu pc seu bosta";
+        olhaquelegal = 67;
+        quebacana = 69;
+        nome_foda = "Matheus vc está banido";
+        olhaquelegal = 3;
+        quebacana = 22;
+        x=6;
+        y=9;
+
+        System.out.println(nome_foda);
+        System.out.print(olhaquelegal);
+        System.out.print(":");
+        System.out.println(quebacana);
+        System.out.println("Quantos minutos tem para a variavel hora somado aos minutos da variavel minutos?");
+        System.out.println((olhaquelegal * 60) + quebacana);
+        System.out.println("Fração de hora que a var minuto representa");
+        System.out.println(quebacana/60);
+        System.out.println("Porcentagem de hora que a var minuto representa");
+        System.out.println((quebacana*100)/60);
+    }
+}
